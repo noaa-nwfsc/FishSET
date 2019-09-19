c784d3321d563d367413ef566f0f4c84d7d0a3c4
Melanie.Harsch <1514847483@mil>
2019-09-19 12:49:06 -0700

    Removed unnecessary code

--- R/zzz.R
+++ R/zzz.R
@@ -2,15 +2,6 @@
 #' 
 .onLoad <- function(libname, pkgname) {
   packageStartupMessage('Welcome to FishSET \n If you would like to track or share your work, we recommend running scripts in a R notebook file.')
-  if (!file.exists('Logs')){
-    dir.create("Logs")
-  }
 }
 
-
-.onAttach <- function(libname, pkgname) {
-  if (!file.exists('Logs')){
-    dir.create("Logs")
-  }
-}
 
