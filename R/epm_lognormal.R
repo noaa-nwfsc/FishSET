b8628d8f920d77a17597ad80fde7dafadb1271b8
Melanie Harsch <melanie.harsch@noaa.gov>
2020-05-29 14:04:59 -0700

    Function description
    Made likelihood functions internal as users are unlikely to need to directly call them.

--- R/epm_lognormal.R
+++ R/epm_lognormal.R
@@ -50,6 +50,7 @@ epm_lognormal <- function(starts3, dat, otherdat, alts, project, expname, mod.na
     #' @param mod.name Name of model run for model result output table
     #' @return ld: negative log likelihood
     #' @export
+    #' @keywords internal
     #' @examples
     #' \dontrun{
     #' data(zi)
