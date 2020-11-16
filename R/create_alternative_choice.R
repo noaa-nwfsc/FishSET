19e16734d06ee6731bc74f682773d0b224f7bf0b
Melanie Harsch <melanie.harsch@noaa.gov>
2020-11-16 10:02:14 -0800

    Updated to simplify for users. Revised text to make easier to follow and allow users to use centroids already calculated

--- R/create_alternative_choice.R
+++ R/create_alternative_choice.R
@@ -1,34 +1,48 @@
-#' Create alternative choice matrix
+#' Create alternative choice list
 #'
-#' Required step. Creates a list containing information on how alternative fishing choices should be defined.  Output is called by \code{\link{make_model_design}}. See function details for more information on output. Run this function before running models.  Output is saved to the FishSET database.
+#' Required step. Creates a list defining how alternative fishing choices should be defined. Output is saved to the FishSET database.
+#'  Run this function before running models.  
 #'
-#' @param dat  Main data frame containing data on hauls or trips.
-#' Table in FishSET database should contain the string `MainDataTable`.
-#' @param gridfile Spatial data containing information on fishery management or regulatory zones.
-#' Shape, json, geojson, and csv formats are supported. \code{gridfile} should be NULL if \code{cat}
-#' is a variable in the primary dataset.
-# @param case Centroid='Centroid of Zonal Assignment', Port, Other
-#' @param min.haul Numeric, minimum number of hauls. Zones with fewer hauls than the \code{min.haul}
-#' value will not be included in model data.
-#' @param hull.polygon Used in \code{\link{assignment_column}} function. Creates polygon using convex
-#' hull method. Required if zonal assignments in \code{dat} should be identified
-#' and \code{gridfile} is not NULL.
-#' @param alt_var String, identifies how to find lat/lon for starting point (must have a lat/lon associated with it).
-#' \code{alt_var} maybe the ‘centroid of zonal assignment’, a port variable or a lat/lon variable in the primary dataset.
-#' If a port variable is defined, a corresponding port table must exist which contains the port name and the latitude and
-#' longitude of each port.
-#' @param occasion Identifies how to find lat/lon for alternative choices. Occasion may be the centroid of zonal assignment \code{"Centroid"},
-#'  a port variable or a lat/lon variable in the primary dataset. If a port variable is defined, a corresponding port table
-#'   must exist which contains the port name and the latitude and longitude of each port.
+#' @param dat  Required, main data frame containing data on hauls or trips.
+#'   Table in FishSET database should contain the string `MainDataTable`.
+#' @param project Required, name of project
+#' @param alt_var String, identifies how to find lat/lon for starting point (must have a lon/lat associated with it).
+#' \code{alt_var} may be the ‘centroid of zonal assignment’, a port variable or lon/lat variable(s) in the primary dataset.
+#' If a port variable is defined, a corresponding port table must exist which contains the port name and the longitude and
+#' latitude of each port. For lon/lat variables, longitude must be specified first.
+#' @param occasion Identifies how to find lat/lon for alternative choices. Occasion may be the centroid of zonal assignment 
+#'  \code{"Centroid"}, or lon/lat variables in the primary dataset. Longitude must be specified first.
+#' @param griddedDat Data must contain a variable that varies by the spatial dataset \code{gridfile}.
+#'  First variable in \code{griddedDat} should match a column in \code{dat}. The remaining columns should match the
+#'  zone IDs in the \code{gridfile}.
 #' @param dist.unit String, how distance measure should be returned.
-#' Choices are \code{"meters"} or \code{"M"}, \code{"kilometers"} or \code{"KM"}, or \code{"miles"}. Defaults to miles.
+#'  Choices are \code{"meters"} or \code{"M"}, \code{"kilometers"} or \code{"KM"}, \code{"miles"}, or units of 
+#'  \code{griddedDat}. Defaults to miles.
+#' @param min.haul Required, numeric, minimum number of hauls. Zones with fewer hauls than the \code{min.haul}
+#' value will not be included in model data.
+#' @param cat Required, variable in either \code{dat} or \code{gridfile} that identifies the individual areas or zones.
+#'  If \code{cat} is a variable in \code{dat} that identifies zone assignments for each occurrence record,
+#'   set \code{gridfile} to NULL. Otherwise, if \code{gridfile} is class sf, \code{cat} should be the name 
+#'   of the list containing information on zones.
+#' @param gridfile Required, data file or character. 
+#'   \code{gridfile} is a spatial data file containing information on fishery management or regulatory zones boundaries.
+#'   Shape, json, geojson, and csv formats are supported. geojson is the preferred format. json files must be converted
+#'   into geoson. This is done automatically when the file is loaded with \code{\link{read_dat}} with \code{\link{is.map}} set to true.
+#'   \code{gridfile} cannot, at this time, be loaded from the FishSET database. \cr
+#'   \code{lon.dat}, \code{lat.dat}, \code{lon.grid}, and \code{lat.grid} are required for specific \code{gridfile} file formats. \cr
+#'  \code{cat} must be specified for all file formats.
+#'  If a zonal centroid table exists in the FishSET database and a zonal assignment column 
+#'  exists in \code{dat} then \code{gridfile} may be a table from the FishSET database or a data file. If \code{gridfile} should 
+#'  come from the FishSET database, it should be the name of the original file name, in quotes. For example, 'NMFS_zones_polygons'.
+#'  \code{cat} would then be the name of the column in \code{dat} containing fishing zone assignments.
+# @param case Centroid='Centroid of Zonal Assignment', Port, Other
+#' @param hull.polygon Used in \code{\link{assignment_column}} function. Creates polygon using convex
+#'   hull method. Required if zonal assignments in \code{dat} should be identified
+#'   and \code{gridfile} is not NULL.
 #' @param lon.dat Longitude variable from \code{dat}. Required if zonal assignments in \code{dat}
-#' should be identified and \code{gridfile} is not NULL.
+#'   should be identified and \code{gridfile} is not NULL.
 #' @param lat.dat Latitude variable from \code{dat}. Required if zonal assignments in \code{dat}
-#' should be identified and \code{gridfile} is not NULL.
-#' @param cat Variable in either \code{dat} or \code{gridfile} that identifies the individual areas or zones.
-#'  If \code{cat} is a variable in \code{dat} that identifies zone assignments for each occurrence record, set \code{gridfile} to NULL.
-#'  Otherwise, if \code{gridfile} is class sf, `cat` should be name of list containing information on zones.
+#'   should be identified and \code{gridfile} is not NULL.
 #' @param lon.grid Variable or list from \code{gridfile} containing longitude data. Required for csv files.
 #'  Leave as NULL if \code{gridfile} is a shape or json file, Required if zonal assignments
 #'  in \code{dat} should be identified and \code{gridfile} is not NULL.
@@ -40,22 +54,23 @@
 #' @param closest.pt Logical, if true, zone ID identified as the closest polygon to the point.
 #'   Called in \code{\link{assignment_column}}. Required if zonal assignments for observations in \code{dat}
 #'   should be identified and \code{gridfile} is not NULL.
-#' @param project String, name of project.
-#' @param griddedDat Optional data frame. Data must contain a variable that varies by the spatial dataset \code{gridfile}.
-#'  First variable in \code{griddedDat} should match a column in \code{dat}. The remaining columns should match the
-#'  zone IDs in the \code{gridfile}.
 #' @importFrom DBI dbExecute
 #' @export create_alternative_choice
-#' @details Function the returns alternative choice matrix and saves the output to the FishSET database. The matrix is
-#'   pulled by \code{\link{create_expectations}}, \code{\link{make_model_design}}, and the model run
-#'   function (\code{\link{discretefish_subroutine}}). Function assigns each observation to a zone and identifies
-#'   which zones are to be included based on a minimum number of hauls per trip within a zone. Functions that call the
-#'   alternative choice matrix will exclude observations in zones that do not meet the minimum haul criteria. Function defines
-#'   how start and end points for calculating the distance matrix in \code{\link{make_model_design}}.
-#'   Note that if the alternative choice matrix is modified, the \code{\link{create_expectations}}
-#'   and \code{\link{make_model_design}} functions should also be updated before rerunning the model run
-#'   function (\code{\link{discretefish_subroutine}}).
-#' @return Saves the alternative choice matrix to the FishSET database as a list.
+#' @details Defines the alternative fishing choices. These choices are used to develop the matrix of distances 
+#'   between observed and alternative fishing choices (where they could have fished but did not). 
+#'   The distance matrix is calculated by the \code{\link{make_model_design}} function.
+#'   The distance matrix can come from \code{dat} or from the gridded data frame \code{griddedDat}.
+#'   If the distance matrix is to come from \code{dat}, then \code{alt_var} (observed fishing location) and 
+#'   \code{occasion} (alternative fishing location) must be specified. \code{griddedDat}, if used, must be a variable that varies 
+#'   by the spatial dataset, such as wind speed. Each column must be a unique zone that matches the zones in \code{dat}. \cr
+#'   
+#'   Parts of the alternative choice list are pulled by \code{\link{create_expectations}}, 
+#'   \code{\link{make_model_design}}, and the model run (\code{\link{discretefish_subroutine}}) functions. 
+#'   These output include choices of which variable to use for catch and 
+#'   which zones to include in analyses based on a minimum number of hauls per trip within a zone. 
+#'   Note that if the alternative choice list is modified, the \code{\link{create_expectations}}
+#'   and \code{\link{make_model_design}} functions should also be updated before rerunning models.
+#' @return Saves the alternative choice list to the FishSET database as a list.
 #'   Output includes: \cr
 #' \tabular{rlll}{
 #'         dataZoneTrue: \tab Vector of 0/1 indicating whether the data from that zone is to be included in the model\cr
@@ -66,26 +81,47 @@
 #'         alt_var: \tab Identifies how to find latitude and longitude for starting point\cr
 #'         occasion: \tab Identifies how to find latitude and longitude for alternative choice \cr
 #'         zoneRow: \tab Zones and choices array\cr
-#'         int: \tab Centroid for each zone. Generated from \code{\link{find_centroid}}
+#'         int: \tab Centroid for each zone. Generated from \code{\link{find_centroid}}\cr
+#'         matrix: \tab Distance matrix is alternative choices comes from gridded dataset
 #'         }
 
-create_alternative_choice <- function(dat, project, gridfile = NULL, min.haul,
-                                      alt_var, occasion, dist.unit = "miles", lon.dat, lat.dat, cat,
-                                      hull.polygon = c(TRUE, FALSE), closest.pt = FALSE, lon.grid = NULL, lat.grid = NULL,
-                                      griddedDat = NULL, weight.var = NULL) {
+create_alternative_choice <- function(dat, project, alt_var='centroid', occasion='centroid', griddedDat = NULL, 
+                                      dist.unit = "miles", min.haul=0, gridfile, cat=NULL, 
+                                      lon.dat=NULL, lat.dat=NULL, hull.polygon = FALSE,
+                                      closest.pt = FALSE, lon.grid = NULL, lat.grid = NULL,
+                                      weight.var = NULL) {
   stopanaly <- 0
   case <- "centroid"
+  
   # Call in datasets
-  dataset <- dat
-  dat <- deparse(substitute(dat))
-
+  out <- data_pull(dat)
+  dat <- out$dat
+  dataset <- out$dataset
+  
+  if(is.null(cat)){
+    cat <- 'ZoneID'
+  }
+  
+  
+  if(!is.null(gridfile)){
+    if(is.character(gridfile)){
+      if(table_exists(paste0(gridfile, 'centroid'))==FALSE){
+        warning('Table', paste0(gridfile, 'centroid'),'not found in FishSET database. 
+                Zonal centroid must be defined from a dataset')
+      } else {
+      int <- table_view(paste0(gridfile, 'centroid'))
+      }
+    } else {
   int <- find_centroid(
     dat = dataset, gridfile = gridfile, lon.grid = lon.grid, lat.grid = lat.grid,
     lat.dat = lat.dat, lon.dat = lon.dat, cat = cat, weight.var = weight.var
   )
-
-  if (!is.null(gridfile)) {
-    # if (!FishSET::is_empty(weight.var)) {
+    }
+  } else {
+    warning("Zonal centroid must be defined.")
+  }
+  
+  if (!is.null(gridfile) & !is.character(gridfile)) {
     int.data <- assignment_column(
       dat = dataset, gridfile = gridfile, hull.polygon = hull.polygon,
       lon.grid = lon.grid, lat.grid = lat.grid, lon.dat = lon.dat,
@@ -94,11 +130,18 @@ create_alternative_choice <- function(dat, project, gridfile = NULL, min.haul,
   } else {
     int.data <- dataset
   }
-  if (anyNA(int.data[[cat]]) == TRUE) {
-    warning(paste("No zone identified for", sum(is.na(int.data[[cat]])), "observations. These observations will be removed in future analyses."))
+  
+  if(!is.null(int.data[[cat]])){
+    g <- int.data[[cat]]
+  } else if(!is.null(int.data[['ZoneID']])){
+    g <- int.data[['ZoneID']]
+  }
+  if (anyNA(g) == TRUE) {
+    warning(paste("No zone identified for", sum(is.na(g)), "observations. 
+                  These observations will be removed in future analyses."))
   }
 
-  choice <- data.frame(int.data[[cat]])
+  choice <- data.frame(g)
   startingloc <- if (!"startingloc" %in% int.data) {
     rep(NA, nrow(int.data))
   } else {
@@ -109,16 +152,18 @@ create_alternative_choice <- function(dat, project, gridfile = NULL, min.haul,
     warning("Choice must be defined. Ensure that the zone or area assignment variable (cat parameter) is defined.")
     stopanaly <- 1
   }
-
-  if (case == "Centroid") {
-    B <- as.data.frame(unique(int.data[[cat]][!is.na(int.data[[cat]])])) # unique(unlist(gridInfo['assignmentColumn',,]))
-    C <- match(int.data[[cat]][!is.na(int.data[[cat]])], unique(int.data[[cat]][!is.na(int.data[[cat]])])) #  match(unlist(gridInfo['assignmentColumn',,]), unique(unlist(gridInfo['assignmentColumn',,])))
+  
+  if (case == "centroid") {
+    B <- as.data.frame(unique(g[!is.na(g)])) # unique(unlist(gridInfo['assignmentColumn',,]))
+    C <- match(g[!is.na(g)], unique(g[!is.na(g)]))#  match(unlist(gridInfo['assignmentColumn',,]), unique(unlist(gridInfo['assignmentColumn',,])))
   } else {
     a <- colnames(dataset)[grep("zon|area", colnames(dataset), ignore.case = TRUE)] # find(zp)   #find data that is zonal type
 
     # [B,I,C]=unique([gridInfo.assignmentColumn(~isnan(gridInfo.assignmentColumn)),
     # data(a(v)).dataColumn(~isnan(gridInfo.assignmentColumn),:) ],'rows');%FIXME check that the order of output zones is consistent
-    temp <- cbind(as.character(int.data[[cat]]), dataset[[a[1]]]) # cbind(unlist(gridInfo['assignmentColumn',,]), unlist(dataset[[a]]))
+
+   
+    temp <- cbind(as.character(g), dataset[[a[1]]]) # cbind(unlist(gridInfo['assignmentColumn',,]), unlist(dataset[[a]]))
     B <- unique(temp) # Correct ->> Needs to be lat/long
     C <- match(paste(temp[, 1], temp[, 2], sep = "*"), paste(B[, 1], B[, 2], sep = "*")) #    C <- data(a(v))[dataColumn,'rows']
   }
@@ -137,7 +182,9 @@ create_alternative_choice <- function(dat, project, gridfile = NULL, min.haul,
 
   if (stopanaly == 0) {
     # dataZoneTrue=ismember(gridInfo.assignmentColumn,zoneHist(greaterNZ,3));
-    dataZoneTrue <- cbind(int.data[[cat]] %in% zoneHist[, 3], match(int.data[[cat]], zoneHist[, 3], nomatch = 0))
+
+    dataZoneTrue <- cbind(g %in% zoneHist[, 3], match(g, zoneHist[, 3], nomatch = 0))
+   
     # dataZoneTrue=ismember(gridInfo.assignmentColumn,zoneHist(greaterNZ,3));
     greaterNZ <- which(zoneHist[, 1] >= min.haul) # ifelse(!is.na(zoneHist[, 1]) & zoneHist[, 1] >= 0, 1, 0)
     numOfNecessary <- min.haul
@@ -161,7 +208,7 @@ create_alternative_choice <- function(dat, project, gridfile = NULL, min.haul,
       int = int # centroid data
     )
   }
-  
+
   ### Add gridded data ###
   if (!is.null(griddedDat)) {
     gridVar <- griddedDat
@@ -173,7 +220,13 @@ create_alternative_choice <- function(dat, project, gridfile = NULL, min.haul,
 
     int <- noquote(gsub("[^0-9]", "", colnames(gridVar)))
 
-    if (any(noquote(gsub("[^0-9]", "", colnames(gridVar))) %in% int.data[[cat]]) == FALSE) {
+    if(!is.null(int.data[[cat]])){
+      g <- int.data[[cat]]
+    } else if(!is.null(int.data[['ZoneID']])) {
+      g <- int.data[['ZoneID']]
+    }
+        
+    if (any(noquote(gsub("[^0-9]", "", colnames(gridVar))) %in% g) == FALSE) {
       warning("Cannot use griddedDat. Column names of griddedDat do not match zone ids in gridfile.")
       st <- 1
     }
@@ -244,8 +297,7 @@ create_alternative_choice <- function(dat, project, gridfile = NULL, min.haul,
     # DBI::dbExecute (fishset_db, "CREATE TABLE IF NOT EXISTS altmatrix (AlternativeMatrix ALT)")
     # DBI::dbExecute (fishset_db, "INSERT INTO altmatrix VALUES (:AlternativeMatrix)", params = list(AlternativeMatrix = list(serialize(Alt, NULL))))
     DBI::dbDisconnect(fishset_db)
-
-
+ 
     create_alternative_choice_function <- list()
     create_alternative_choice_function$functionID <- "create_alternative_choice"
     create_alternative_choice_function$args <- list(
