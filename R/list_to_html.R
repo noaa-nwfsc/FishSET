723a24c705ce69c99b0eeee465ea7136306446f6
Bryce McManus <bryce.mcmanus@noaa.gov>
2021-11-17 14:23:12 -0800

    switch to rlang::is_bare_list

--- R/list_to_html.R
+++ R/list_to_html.R
@@ -66,7 +66,7 @@ list_html_r <- function(l) {
   #' @keywords internal
   #' @return A list of HTML list item strings. 
   
-  nms <- names(l)
+  nms <- names(l) # what if no names?
   
   purrr::map2(l, nms, function(x, y) {
 
@@ -89,6 +89,7 @@ list_to_html <- function(l) {
   #' 
   #' @param l A list.
   #' @importFrom shiny tags
+  #' @importFrom rlang is_bare_list
   #' @export
   #' @return An un-ordered HTML list.
   #' @seealso \code{\link{simplify_list}} \code{\link{collapse_leaf_r}} 
@@ -100,7 +101,8 @@ list_to_html <- function(l) {
   #'   list_to_html(l)
   #' }
   
-  if (is.list(l) & !is.data.frame(l)) {
+  # if (is.list(l) & !is.data.frame(l)) {
+  if (rlang::is_bare_list(l)) {
     
     l <- simplify_list(l)
     
