20da50113958d3822417e194cc0980e748830233
Bryce McManus <bryce.mcmanus@noaa.gov>
2022-05-13 13:37:45 -0700

    Add TODO, reformat doc

--- R/check_model_data.R
+++ R/check_model_data.R
@@ -1,18 +1,25 @@
 #' Check for common data quality issues affecting modeling functions
 #'
-#' @param dat Primary data containing information on hauls or trips. Table in FishSET database contains the string 'MainDataTable'.
+#' @param dat Primary data containing information on hauls or trips. Table in 
+#'   FishSET database contains the string 'MainDataTable'.
 #' @param project Project name. 
 #' @param uniqueID Variable in \code{dat} containing unique occurrence identifier.
-#' @param save.file Logical, if TRUE and no data issues are identified, the dataset is saved to the FishSET database. Defaults to TRUE.
-#' @return Returns statements of data quality issues in the data. Saves table to FishSET database.
+#' @param save.file Logical, if TRUE and no data issues are identified, the dataset 
+#'   is saved to the FishSET database. Defaults to \code{TRUE}.
+#' @return Returns statements of data quality issues in the data. Saves table to 
+#'   FishSET database.
 #' @export check_model_data
-#' @description Check the primary dataset for NAs, NaNs, Inf, and that each row is a unique choice occurrence
-#' @details It is best to check the data for NAs, NaNs and Inf, and that each row is a unique choice occurrence 
-#' after data creation functions have been run but before making the model design file (\code{make_model_design}). 
-#' These steps should be taken even if the data passed earlier data verification checks, as data quality issues can arise in the creation or 
-#' modification of data. Model functions may fail or return inaccurate results if data quality issues exist. 
-#' The integrated data will not save if any of these issues are in the dataset. If data passes all tests, then data
-#' will be saved in the FishSET database with the prefix ‘final’. The data index table will also be updated and saved.
+#' @description Check the primary dataset for NAs, NaNs, Inf, and that each row 
+#'   is a unique choice occurrence
+#' @details It is best to check the data for NAs, NaNs and Inf, and that each row 
+#'   is a unique choice occurrence after data creation functions have been run but 
+#'   before making the model design file (\code{make_model_design}). These steps 
+#'   should be taken even if the data passed earlier data verification checks, as 
+#'   data quality issues can arise in the creation or modification of data. Model 
+#'   functions may fail or return inaccurate results if data quality issues exist. 
+#'   The integrated data will not save if any of these issues are in the dataset. 
+#'   If data passes all tests, then data will be saved in the FishSET database with 
+#'   the prefix ‘final’. The data index table will also be updated and saved.
 #' @examples
 #' \dontrun{
 #' check_model_data(MainDataTable, uniqueID = "uniqueID_Code", save.file = TRUE)
@@ -50,6 +57,7 @@ check_model_data <- function(dat, project, uniqueID, save.file = TRUE) {
 
   # is.inf
   if (any(qaqc_helper(dataset, "Inf"))) {
+    
     inf_cols <- qaqc_helper(dataset, "Inf", "names")
     inf_msg <- paste("\nInfinite values are present in", paste(inf_cols, collapse = ", "))
     cat(inf_msg, file = tmp, append = TRUE)
@@ -58,6 +66,7 @@ check_model_data <- function(dat, project, uniqueID, save.file = TRUE) {
   }
 
   # unique rows
+  # TODO: Make this work for trip and haul level, multiple ID cols (switch to dplyr::distict)
   if (length(dataset[[uniqueID]]) != length(unique(dataset[[uniqueID]]))) {
     
     unique_msg <- paste("\nThe uniqueID variable should define the length of unique",
@@ -88,7 +97,9 @@ check_model_data <- function(dat, project, uniqueID, save.file = TRUE) {
   if (end) warning("At least one test did not pass. Data set will not be saved.")
   
   if (end == FALSE) {
+    
     if (save.file == TRUE) {
+      
       cat(paste("\nModified data set saved to fishset_db database"), file = tmp, append = TRUE)
       fishset_db <- suppressWarnings(DBI::dbConnect(RSQLite::SQLite(), locdatabase(project = project)))
       on.exit(DBI::dbDisconnect(fishset_db), add = TRUE)
@@ -97,6 +108,7 @@ check_model_data <- function(dat, project, uniqueID, save.file = TRUE) {
       DBI::dbWriteTable(fishset_db, single_sql, dataset, overwrite = TRUE)
     }
   }
+  
   # logging function information
   check_model_data_function <- list()
   check_model_data_function$functionID <- "check_model_data"
