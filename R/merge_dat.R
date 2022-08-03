2ec24e8c9938a6a6214a8a15f36478005c12e63e
Bryce McManus <bryce.mcmanus@noaa.gov>
2022-08-03 15:33:21 -0700

    change arg order
    add column_check(), swtich to stop()

--- R/merge_dat.R
+++ R/merge_dat.R
@@ -1,19 +1,19 @@
 
 
-merge_dat <- function(dat, project, other, main_key, other_key, other_type = NULL, 
+merge_dat <- function(dat, other, project, main_key, other_key, other_type, 
                       merge_type = "left") {
   #'
   #' Merge data tables using a left join
   #'
-  #' @param dat Primary data containing information on hauls or trips. Table in the FishSET
-  #' database contains the string 'MainDataTable'.
+  #' @param dat Primary data containing information on hauls or trips. Table in 
+  #'   the FishSET database contains the string 'MainDataTable'.
+  #' @param other A second data table to join to MainDataTable. Use string if 
+  #'   referencing a table saved in the FishSET database. 
   #' @param project Project name. 
-  #' @param other A second data table to join to MainDataTable. Use string if referencing 
-  #'   a table saved in the FishSET database. 
-  #' @param main_key String, name of column(s) in MainDataTable to join by. The number
-  #'   of columns must match "other_key".  
-  #' @param other_key String, name of column(s) in the \code{other} table to join by. The number
-  #'   of columns must match "main_key".
+  #' @param main_key String, name of column(s) in MainDataTable to join by. The 
+  #'   number of columns must match \code{other_key}.  
+  #' @param other_key String, name of column(s) in the \code{other} table to join 
+  #'   by. The number of columns must match \code{main_key}.
   #' @param other_type String, the type of secondary data being merged. Options 
   #'   include "aux" (auxiliary), "grid" (gridded), "spat" (spatial), and "port". 
   #' @param merge_type String, the type of merge to perform. \code{"left"} keeps all
@@ -24,13 +24,14 @@ merge_dat <- function(dat, project, other, main_key, other_key, other_type = NUL
   #' @importFrom stats setNames
   #' @export
   #' @details 
-  #'   This function merges two datasets using a left join: all columns and rows from the
-  #'   MainDataTable are kept while only matching columns and rows from the secondary table 
-  #'   are joined. 
+  #'   This function merges two datasets using a left join: all columns and rows 
+  #'   from the MainDataTable are kept while only matching columns and rows from 
+  #'   the secondary table are joined. 
   #' @examples
   #' \dontrun{
-  #'  pollockMainDataTable <- merge_dat("pollockMainDataTable", "pollock", 
-  #'           "pollockPortTable",  main_key = "PORT_CODE", other_key = "PORT_CODE")
+  #'  pollockMainDataTable <- 
+  #'     merge_dat("pollockMainDataTable", "pollockPortTable", "pollock", 
+  #'               main_key = "PORT_CODE", other_key = "PORT_CODE")
   #' } 
 
   # pull main data
@@ -38,22 +39,22 @@ merge_dat <- function(dat, project, other, main_key, other_key, other_type = NUL
   dataset <- out$dataset
   dat <- parse_data_name(dat, "main", project)
   
-  
   # pull secondary data
   other_out <- data_pull(other, project)
   other_dat <- other_out$dataset
+  
+  # check column names exist
+  column_check(dataset, main_key)
+  column_check(other_dat, other_key)
   
   if (other_type == "aux") other <- parse_data_name(other, "aux", project)
   else if (other_type == "grid") other <- parse_data_name(other, "grid", project)
   else if (other_type == "port") other <- parse_data_name(other, "port", project)
   else if (other_type == "spat") other <- parse_data_name(other, "spat", project)
-  
-  end <- FALSE 
 
   if (length(main_key) != length(other_key)) {
     
-    warning("Key lengths must match.")
-    end <- TRUE
+    stop("Key lengths must match.", call. = FALSE)
     
   } else {
     
@@ -88,67 +89,61 @@ merge_dat <- function(dat, project, other, main_key, other_key, other_type = NUL
         
       }, FUN.VALUE = character(1))
       
-      warning(paste0(class_error, collapse = "\n"))
-      
-      end <- TRUE
+      stop(paste0(class_error, collapse = "\n"), call. = FALSE)
     }
   }
   
-  if (end == FALSE) {
+  is_factor_char <- function(x) (is.character(x) | is.factor(x)) 
+  
+  # if key is character or factor class, trim white spaces
+  if (any(vapply(dataset[main_key], FUN = is_factor_char, FUN.VALUE = logical(1)))) {
     
-    is_factor_char <- function(x) (is.character(x) | is.factor(x)) 
-    
-    # if key is character or factor class, trim white spaces
-    if (any(vapply(dataset[main_key], FUN = is_factor_char, FUN.VALUE = logical(1)))) {
-      
-      dataset[main_key] <- lapply(main_key, function(x) trimws(dataset[[x]]))
-    }
-    
-    if (any(vapply(other_dat[other_key], FUN = is_factor_char, FUN.VALUE = logical(1)))) {
-      
-      other_dat[other_key] <- lapply(other_key, function(x) trimws(other_dat[[x]]))
-    }
-    
-    if (merge_type == "left") {
-      
-      dataset <-
-        dplyr::left_join(dataset,
-                         other_dat,
-                         by = merge_by,
-                         suffix = c("_main", paste0("_", other_type)))
-    
-    } else if (merge_type == "full") {
-      
-      dataset <-
-        dplyr::full_join(dataset,
-                         other_dat,
-                         by = merge_by,
-                         suffix = c("_main", paste0("_", other_type)))
-    }
-    
-    
-    # Log function
-    merge_dat_function <- list()
-    merge_dat_function$functionID <- "merge_dat"
-    merge_dat_function$args <- list(dat, project, other, main_key, other_key, 
-                                    other_type, merge_type)
-    log_call(project, merge_dat_function)
-    
-    dataset
+    dataset[main_key] <- lapply(main_key, function(x) trimws(dataset[[x]]))
   }
+  
+  if (any(vapply(other_dat[other_key], FUN = is_factor_char, FUN.VALUE = logical(1)))) {
+    
+    other_dat[other_key] <- lapply(other_key, function(x) trimws(other_dat[[x]]))
+  }
+  
+  if (merge_type == "left") {
+    
+    dataset <-
+      dplyr::left_join(dataset,
+                       other_dat,
+                       by = merge_by,
+                       suffix = c("_main", paste0("_", other_type)))
+  
+  } else if (merge_type == "full") {
+    
+    dataset <-
+      dplyr::full_join(dataset,
+                       other_dat,
+                       by = merge_by,
+                       suffix = c("_main", paste0("_", other_type)))
+  }
+  
+  # Log function
+  merge_dat_function <- list()
+  merge_dat_function$functionID <- "merge_dat"
+  merge_dat_function$args <- list(dat, other, project, main_key, other_key, 
+                                  other_type, merge_type)
+  log_call(project, merge_dat_function)
+  
+  dataset
 }
 
 
-split_dat <- function(dat, project, aux = NULL, split_by = NULL, key, output = "main") {
+split_dat <- function(dat, aux = NULL, project, split_by = NULL, key, output = "main") {
   #'
   #' Separate secondary data table from MainDataTable
   #' 
   #' @param dat Primary data containing information on hauls or trips. Table in the FishSET
   #'   database contains the string 'MainDataTable'.
-  #' @param project String, name of project.
   #' @param aux Auxiliary data table in fishset_db or environment. Use string if referencing 
   #'   a table saved in the FishSET database. The column names from "aux"
   #'   will be used to find and separate the auxiliary table from the MainDataTable.
+  #' @param project String, name of project.
   #' @param split_by String, columns in MainDataTable to split by. These columns will be 
   #'   separated from MainDataTable. Must contain values from "key". 
   #' @param key String, the column(s) that link the main and auxiliary data tables. If using "aux" method, "key" 
@@ -173,7 +168,6 @@ split_dat <- function(dat, project, aux = NULL, split_by = NULL, key, output = "
   dataset <- out$dataset
   dat <- parse_data_name(dat, "main", project)
   
- 
   aux_dat <- NULL
   
   if (!is.null(aux)) { 
@@ -183,12 +177,13 @@ split_dat <- function(dat, project, aux = NULL, split_by = NULL, key, output = "
     aux <- parse_data_name(aux, 'aux', project)
   }
   
-  end <- FALSE
+  # TODO: allow key arg to work w/ differently named keys (ex: "PORT_NAME" and "port")
+  # This functionality exists in merge_dat() so should also be included here
   
   if (is.null(aux_dat) & is.null(split_by)) {
     
-    warning("Both 'aux' and 'split_by' arguments are missing. Please select a splitting option.")
-    end <- TRUE
+    stop("Both 'aux' and 'split_by' arguments are missing. Please select a splitting option.",
+         call. = FALSE)
   }
   
   main_cols <- colnames(dataset)
@@ -196,57 +191,54 @@ split_dat <- function(dat, project, aux = NULL, split_by = NULL, key, output = "
   if (!is.null(aux_dat)) {
     
     aux_cols <- colnames(aux_dat) 
-    end <- split_check(main_cols, aux_cols, key, "aux")
+    split_check(main_cols, aux_cols, key, "aux")
   
   } else if (!is.null(split_by)) {
     
-    end <- split_check(main_cols, split_by, key, "split_by")
+    split_check(main_cols, split_by, key, "split_by")
+  }
+
+  # determine split by aux colnames
+  if (!is.null(aux_dat)) {
+    
+    matched_cols <- aux_cols[aux_cols %in% main_cols]
+    matched_cols <- matched_cols[!(matched_cols %in% key)]
+    
+    dataset <- dataset[, !(main_cols %in% matched_cols)]
+
+  } else if (!is.null(split_by)) {
+
+    if (any(output %in% c("aux", "both"))) {
+      
+      aux_dat <- dataset[split_by] 
+      
+      # return unique rows
+      aux_dat <- dplyr::distinct(aux_dat)
+    }
+      
+    # remove all aux columns from main data (except key) 
+    split_by <- split_by[!(split_by %in% key)]
+    dataset <- dataset[, !(main_cols %in% split_by)]
   }
   
-  if (end == FALSE) {
-  
-    # determine split by aux colnames
-    if (!is.null(aux_dat)) {
-      
-      matched_cols <- aux_cols[aux_cols %in% main_cols]
-      matched_cols <- matched_cols[!(matched_cols %in% key)]
-      
-      dataset <- dataset[, !(main_cols %in% matched_cols)]
-  
-    } else if (!is.null(split_by)) {
-  
-      if (any(output %in% c("aux", "both"))) {
-        
-        aux_dat <- dataset[split_by] 
-        
-        # return unique rows
-        aux_dat <- dplyr::distinct(aux_dat)
-      }
-        
-      # remove all aux columns from main data (except key) 
-      split_by <- split_by[!(split_by %in% key)]
-      dataset <- dataset[, !(main_cols %in% split_by)]
-    }
+  # Log function
+  split_dat_function <- list()
+  split_dat_function$functionID <- "split_dat"
+  split_dat_function$args <- list(dat, aux, project, split_by, key, output)
+  log_call(project, split_dat_function)
     
-    # Log function
-    split_dat_function <- list()
-    split_dat_function$functionID <- "split_dat"
-    split_dat_function$args <- list(dat, project, aux, split_by, key, output)
-    log_call(project, split_dat_function)
-      
-    if (output == "main") {
-      
-      dataset
+  if (output == "main") {
     
-    } else if (output == "aux") {
-      
-      aux_dat
+    dataset
+  
+  } else if (output == "aux") {
     
-    } else if (output == "both") {
-      
-      list("main" = dataset, "aux" = aux_dat)
-    }
-  } 
+    aux_dat
+  
+  } else if (output == "both") {
+    
+    list("main" = dataset, "aux" = aux_dat)
+  }
 }                                                                               
 
 
@@ -261,36 +253,26 @@ split_check <- function(main, split, key, arg) {
   #' @keywords internal
   #' @export 
   
-  end <- FALSE
-  
   if (!all(split %in% main)) {
     
     # note which colnames aren't in main data
     not_found <- split[!(split %in% main)]
-    #warning(paste("Column(s)", paste(not_found, collapse = ", "), "in", arg, "are not in main data."))
-    warning(paste0("Main data does not contain column(s) ", paste(not_found, collapse = ", "), 
-                  ". Did you specify the correct columns in '", arg, "'?"))
-    end <- TRUE
+    stop(paste0("Main data does not contain column(s) ", paste(not_found, collapse = ", "), 
+                ". Did you specify the correct columns in '", arg, "'?"), call. = FALSE)
   }
   
   if (!all(key %in% main)) {
     
     # note which keys aren't in main data
     not_found <- key[!(key %in% main)]
-    #warning(paste("Column(s)", paste(not_found, collapse = ", "), "in 'key' are not in main data."))
-    warning(paste0("Main data does not contain column(s) ", paste(not_found, collapse = ", "), 
-                   ". Did you specify the correct columns in 'key'?"))
-    end <- TRUE
+    stop(paste0("Main data does not contain column(s) ", paste(not_found, collapse = ", "), 
+                ". Did you specify the correct columns in 'key'?"), call. = FALSE)
   }
   
   if (!all(key %in% split)) {
     
     not_found <- key[!(key %in% split)]
-    #warning(paste0("Column(s) ", paste(not_found, collapse = ", "), " in 'key' are not in ", arg, "."))
-    warning(paste0("'", arg, "' does not contain column(s) ", paste(not_found, collapse = ", "), 
-                   ". Did you specify the correct columns in 'key'?"))
-    end <- TRUE
+    stop(paste0("'", arg, "' does not contain column(s) ", paste(not_found, collapse = ", "), 
+                ". Did you specify the correct columns in 'key'?"), call. = FALSE)
   }
-  
-  end
 }
