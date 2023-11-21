335e062bdaa0a5f44e1b03c528661a942ac7c52d
Paul Carvalho <paul.carvalho@noaa.gov>
2023-11-21 20:50:31 +0000

    Added run prediction function and output tables/figs to shiny

--- R/predict_map.R
+++ R/predict_map.R
@@ -1,6 +1,6 @@
 #' Map of predicted probabilities
 #'
-#' Create a map showing predicted probabilities by zone for a chosen policy scenario
+#' Create a map showing predicted probabilities by zone
 #'
 #' @param project Name of project
 #' @param policy.name Name of policy scenario
@@ -10,8 +10,13 @@
 #'  a csv file to an `sf` object. To upload your spatial data to the FishSETFolder 
 #'  see [load_spatial()].
 #' @param zone.spat Name of zone ID column in `spat`.
+#' @param outsample Logical, indicating if \code{predict_map()} is being used for creating map of out-of-sample
+#' predicted fishing probabilities \code{outsample = TRUE} or policy scenario \code{outsample = FALSE}.
+#' @param outsample_pred A dataframe with fishing location and predicted probabilities for out-of-sample data.
+#' \code{outsample_pred = NULL} by default and when plotting policy scenarios.
 #' @return A map showing predicted probabilities
-#' @details This function requires that model and prediction output tables exist in the FishSET database.
+#' @details This function requires that model and prediction output tables exist in the FishSET database when 
+#' plotting policy scenario maps.
 #' @import ggplot2
 #' @import dplyr
 #' @import sf
@@ -24,53 +29,58 @@
 #'
 #' }
 
-predict_map <- function(project, policy.name, spat, zone.spat){
+predict_map <- function(project, policy.name = NULL, spat, zone.spat, outsample = FALSE, outsample_pred = NULL){
   
-  # Create connection to database and remove connection on exit of this function
-  fishset_db <- DBI::dbConnect(RSQLite::SQLite(), locdatabase(project = project))
-  on.exit(DBI::dbDisconnect(fishset_db), add = TRUE)
-  
-  # Load prediction outputs -------------------------------------------------------------------------------------------------
-  # make sure prediction table exist in the database
-  # TODO: If this is running in the Shiny and one or more of the tables above do not exist then show error message and stop running function here
-  tryCatch({
-    model_output <- unserialize_table(paste0(project, "ModelOut"), project)},
+  # Policy map ----------------------------------------------------------------------------------------------------------------
+  if(!outsample){
+    # Create connection to database and remove connection on exit of this function
+    fishset_db <- DBI::dbConnect(RSQLite::SQLite(), locdatabase(project = project))
+    on.exit(DBI::dbDisconnect(fishset_db), add = TRUE)
     
-    error = function(err){message(paste0("Model output table not found in ", project))}
-  )
-  tryCatch({
-    pred_output <- unserialize_table(paste0(project, "predictOutput"), project)},
+    ## Load prediction outputs -------------------------------------------------------------------------------------------------
+    # make sure prediction table exist in the database
+    # TODO: If this is running in the Shiny and one or more of the tables above do not exist then show error message and stop running function here
+    tryCatch({
+      model_output <- unserialize_table(paste0(project, "ModelOut"), project)},
+      
+      error = function(err){message(paste0("Model output table not found in ", project))}
+    )
+    tryCatch({
+      pred_output <- unserialize_table(paste0(project, "PredictOutput"), project)},
+      
+      error = function(err){message(paste0("Prediction output table not found in ", project))}  
+    )
     
-    error = function(err){message(paste0("Prediction output table not found in ", project))}  
-  )
-  
-  # Get a list of model names from prediction output scenario names
-  predict_n <- unlist(lapply(pred_output, function(x) x$scenario.name))
-  mod_n <- unique(sapply(strsplit(predict_n, split = " "), "[", 1))
-  
-  if(policy.name %in% mod_n){
-    # Get the index for the first prediction output for the model
-    ind <- grep(policy.name, predict_n)[1]
+    # Get a list of model names from prediction output scenario names
+    predict_n <- unlist(lapply(pred_output, function(x) x$scenario.name))
+    mod_n <- unique(sapply(strsplit(predict_n, split = " "), "[", 1))
     
-    # Get predicted probabilities by zone for the model
-    predProbs <- pred_output[[ind]]$prob[, 1]/100
-    probs_df <- data.frame(ZoneID = as.character(pred_output[[ind]]$zoneID), 
-                           Probability = predProbs)
+    if(policy.name %in% mod_n){
+      # Get the index for the first prediction output for the model
+      ind <- grep(policy.name, predict_n)[1]
+      
+      # Get predicted probabilities by zone for the model
+      predProbs <- pred_output[[ind]]$prob[, 1]/100
+      probs_df <- data.frame(ZoneID = as.character(pred_output[[ind]]$zoneID), 
+                             Probability = predProbs)
+      
+    } else if (policy.name %in% predict_n) {
+      # Get index for the prediction output for the policy
+      ind <- which(predict_n %in% policy.name)
+      
+      # Get predicted probabilities by zone for the policy
+      predProbs <- pred_output[[ind]]$prob[, 2]/100
+      probs_df <- data.frame(ZoneID = as.character(pred_output[[ind]]$zoneID), 
+                             Probability = predProbs)
+      
+      # Get zone ID for closured zones
+      closure <- pred_output[[ind]]$zoneIdIn
+    }
+  } else {
     
-  } else if (policy.name %in% predict_n) {
-    # Get index for the prediction output for the policy
-    ind <- which(predict_n %in% policy.name)
-    
-    # Get predicted probabilities by zone for the policy
-    predProbs <- pred_output[[ind]]$prob[, 2]/100
-    probs_df <- data.frame(ZoneID = as.character(pred_output[[ind]]$zoneID), 
-                           Probability = predProbs)
-    
-    # Get zone ID for closured zones
-    closure <- pred_output[[ind]]$zoneIdIn
+  # Out-of-sample prediction map --------------------------------------------------------------------------------------------
+    probs_df <- outsample_pred
   }
-  
-  
   
   # Create map --------------------------------------------------------------------------------------------------------------
   
@@ -81,7 +91,8 @@ predict_map <- function(project, policy.name, spat, zone.spat){
   spatdat[[zone.spat]] <- as.character(spatdat[[zone.spat]])
   
   # Rename and re-type probs_df zone column
-  names(probs_df)[1] <- zone.spat
+  names(probs_df) <- c(zone.spat, "Probability")
+  probs_df[,1] <- as.character(probs_df[,1])
   
   # Merge spatial dataset with predicted probabilities
   spat_join <- dplyr::left_join(spatdat[zone.spat], probs_df, by = zone.spat)
@@ -98,6 +109,11 @@ predict_map <- function(project, policy.name, spat, zone.spat){
   # create a bbox using zones that exist in dat
   z_ind <- spatdat[[zone.spat]] %in% unlist(probs_df[zone.spat])
   bbox <- sf::st_bbox(spatdat[z_ind, ]) # keeps shifted long
+  
+  # If this is na then input settings not correct
+  if(is.na(bbox["xmin"])){
+    return(1)
+  }
   
   # world2 uses 0 - 360 lon format
   base_map <- ggplot2::map_data(map = ifelse(shift_long(spatdat), "world2", "world"),
@@ -130,7 +146,7 @@ predict_map <- function(project, policy.name, spat, zone.spat){
     ggplot2::geom_sf(data = base_map) +  
     ggplot2::geom_sf(data = spat_join, 
                      ggplot2::aes(fill = !!var_sym()), color = "black", alpha = .8) +
-    ggplot2::coord_sf(xlim = c(bbox[1], bbox[3]), ylim = c(bbox[2], bbox[4]),
+    ggplot2::coord_sf(xlim = c(bbox[1]-1, bbox[3]+1), ylim = c(bbox[2]-1, bbox[4]+1),
                       expand = TRUE) +
     ggplot2::binned_scale(aesthetics = "fill",
                           scale_name = "stepsn", 
