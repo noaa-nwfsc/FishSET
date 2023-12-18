0edd2eada98da747a8b647b37d9291e1a92723de
Paul Carvalho <paul.carvalho@noaa.gov>
2023-12-18 22:33:58 +0000

    EPM Weibull model running in console version

--- R/epm_weibull.R
+++ R/epm_weibull.R
@@ -3,7 +3,7 @@ epm_weibull <- function(starts3, dat, otherdat, alts, project, expname, mod.name
   #'
   #' @param starts3 Starting values as a vector (num). For this likelihood,
   #'     the order takes: c([catch-function parameters], [travel-distance
-  #'     parameters], [catch sigma(s)], [scale parameter]). \cr \cr
+  #'     parameters], [shape parameter], [catch sigma(s)]). \cr \cr
   #'     The catch-function and travel-distance parameters are of length (# of
   #'     catch-function variables)*(k) and (# of travel-distance variables)
   #'     respectively, where (k) equals the number of alternatives. The catch
@@ -92,85 +92,134 @@ epm_weibull <- function(starts3, dat, otherdat, alts, project, expname, mod.name
   #' }
   #'
 
+  
+  ##
+  ## Set up variables ----
+  ##
+  # Get number of observations
   obsnum <- dim(as.data.frame(otherdat$griddat))[1]
-
+  
+  # Format choice occasion-specific variables (G_im in R user manual equation) that interact with alternative-specific parameters (beta_jm in R user manual equation)
   griddat <- as.matrix(do.call(cbind, otherdat$griddat))
-  gridnum <- dim(griddat)[2]
-  griddat <- matrix(apply(griddat, 2, function(x) rep(x, times = alts)), obsnum, gridnum * alts)
+  gridnum <- dim(griddat)[2] # number of occasion-specific variables
+  griddat <- matrix(apply(griddat, 2, function(x) rep(x, times = alts)), obsnum, gridnum * alts) # replicate the vector for each variable by the number of alternatives and cbind
+  
+  # Format alternative-invariant variables (T_in in R user manual) that interact with distance (D_ij) and alternative-invariant parameters (gamma_n)
   intdat <- as.matrix(do.call(cbind, otherdat$intdat))
   intnum <- dim(intdat)[2]
 
+  # Format price data
   pricedat <- as.matrix(unlist(otherdat$pricedat))
 
+  
+  ##
+  ## Parse initial values and coefficients ----
+  ##
+  # Format initial values and coefficients
   starts3 <- as.matrix(starts3)
   gridcoef <- as.matrix(starts3[1:(gridnum * alts), ])
+  intcoef <- as.matrix(starts3[((gridnum * alts) + 1):((gridnum * alts) + intnum), ])
 
-  intcoef <- as.matrix(starts3[(((gridnum * alts) + intnum) - intnum + 1):((gridnum * alts) + intnum), ])
-
+  # Format shape parameter (k in R user manual) and catch scale parameter (sigma_epsilon in R user manual)
+  # if number of parameters before scale parameter is not equal to number of alts, use first parameter for shape (k)
   if ((dim(starts3)[1] - ((gridnum * alts) + intnum + 1)) == alts) {
     k <- as.matrix(starts3[((gridnum * alts) + intnum + 1):((gridnum * alts) + intnum + alts), ])
-    signum <- alts
+    knum <- alts
+    sig <- as.matrix(starts3[((gridnum * alts) + intnum + alts + 1):length(starts3), ])
   } else {
     k <- as.matrix(starts3[((gridnum * alts) + intnum + 1), ])
-    signum <- 1
+    knum <- 1
+    sig <- as.matrix(starts3[((gridnum * alts) + intnum + 2):length(starts3), ])
   }
-  # if number of parameters before scale parameter is not equal to number of alts, use first parameter as sigma catch
 
-  k <- sqrt(k^2)
+  # Force k to be a positive value
+  k_exp <- exp(k)
 
-  sigmac <- as.matrix(starts3[((gridnum * alts) + intnum + 1 + signum), ])
-  # end of vector
-
+  # sigmac <- as.matrix(starts3[((gridnum * alts) + intnum + 1 + signum), ])
+  # end of coefficient vector
+    
+  ##
+  ## Calculate likelihood for choice component ----
+  ##
+  # beta_jm * G_im
   gridbetas <- (matrix(gridcoef, obsnum, alts * gridnum, byrow = TRUE) * griddat)
   dim(gridbetas) <- c(nrow(gridbetas), alts, gridnum)
+  
+  # SUM(beta_jm * G_im), which is the scale portion of the Weibull function
   gridbetas <- rowSums(gridbetas, dims = 2)
+  
+  # constrain Weibull scale to positive value with exponential function
+  gridbetas_exp <- exp(gridbetas)
 
-  gridmu <- sqrt(gridbetas^2)
+  # Expected mean catch function
+  gridmu <- gridbetas_exp * matrix(gamma((k_exp + 1) / k_exp), obsnum, alts)
 
-  expgridcoef <- gridmu * matrix(gamma(1 + (1 / k)), obsnum, alts)
-
+  # Cost portion of the likelihood
   intbetas <- .rowSums(intdat * matrix(intcoef, obsnum, intnum, byrow = TRUE), obsnum, intnum)
 
-  betas <- matrix(c((expgridcoef * matrix(pricedat, obsnum, alts)), intbetas), obsnum, (alts + 1))
+  # Revenue portion of the likelihood (i.e., price * expected mean catch) followed by cost portion, saved in the same matrix
+  rev_cost_betas <- matrix(c((gridmu * matrix(pricedat, obsnum, alts)), intbetas), obsnum, (alts + 1))
 
-  djztemp <- betas[1:obsnum, rep(1:ncol(betas), each = alts)] * dat[, 3:(dim(dat)[2])]
+  # (Revenue * dummy variable indicating zones fished) and (cost * distance) in a single, large matrix
+  # Note: distances in dat are shifted/sorted, same as choice possibilities, even though the zone columns retain the original order of zones
+  djztemp <- rev_cost_betas[1:obsnum, rep(1:ncol(rev_cost_betas), each = alts)] * dat[, 3:(dim(dat)[2])]
+  
+  # Reformat matrix [d1 = num of obs, d2 = num of alts, d3 = num of alts + 1 (zonal coefficients in revenue portion and cost portion)]
   dim(djztemp) <- c(nrow(djztemp), ncol(djztemp) / (alts + 1), alts + 1)
 
+  # Revenue + cost for each zone
   prof <- rowSums(djztemp, dims = 2)
+  
+  # Relative to the first column so set = 0
   profx <- prof - prof[, 1]
 
-  exb <- exp(profx / matrix(sigmac, dim(prof)[1], dim(prof)[2]))
+  # Exp(Beta*G + Gamma*T*D) for each zone
+  exb <- exp(profx / matrix(sig, dim(prof)[1], dim(prof)[2]))
 
+  # Log-likelihood (for each observation) for choice component of the model
   ldchoice <- (-log(rowSums(exb)))
-
-  yj <- dat[, 1]
-  cj <- dat[, 2]
-
-  if (signum == 1) {
-    empk <- k
+  
+  
+  ##
+  ## Calculate likelihood for catch component ----
+  ##
+  yj <- matrix(dat[, 1]) # actual catch
+  cj <- matrix(dat[, 2]) # actual choice
+  
+  # Get the shape parameter for each observation
+  if (knum == 1) {
+    empk <- k_exp
   } else {
-    empk <- k[cj]
+    empk <- k_exp[cj]
   }
 
+  # Format beta parameters
   empgridbetas <- t(gridcoef)
   dim(empgridbetas) <- c(nrow(empgridbetas), alts, gridnum)
 
+  # Format choice occasion-specific parameters that interact with beta parameters
   empgriddat <- griddat
   dim(empgriddat) <- c(nrow(empgriddat), alts, gridnum)
 
-  empgridmu <- .rowSums(empgridbetas[, cj, ] * empgriddat[, 1, ], obsnum, gridnum)
-  # note grid data same across all alternatives
-
-  empgridmu <- sqrt(empgridmu^2)
-
-  ldcatch <- (matrix((log(empk)), obsnum)) + (matrix((-(empk)), obsnum) * log(empgridmu)) + (matrix((empk - 1), obsnum) * log(yj)) + (-((yj / empgridmu)^(matrix(
-    empk,
-    obsnum
-  ))))
-
+  # Calculate scale portion of the Weibull catch function
+  empgridmu <- .rowSums(empgridbetas[, cj, ] * empgriddat[, 1, ], obsnum, gridnum) # note grid data same across all alternatives
+  
+  # Force values to be positive
+  empgridmu <- exp(empgridmu)
+  
+  # Log-likelihood Weibull function
+  # log(k) + (-k * log(lambda)) + ((k - 1) * log(Y)) + (-((Y / lambda)^(k)))
+  ldcatch <- (matrix((log(empk)), obsnum)) + (matrix((-(empk)), obsnum) * log(empgridmu)) + (matrix((empk - 1), obsnum) * log(yj)) + 
+    (-((yj / empgridmu)^(matrix(empk, obsnum))))
+  
+  # Sum log-likelihoods (for each observation) for catch and choice components of EPM model
   ld1 <- ldcatch + ldchoice
 
-  ld <- -sum(ld1)
+  
+  ##
+  ## Negative log-likelihood ----
+  ##
+  ld <- -(sum(ld1))
 
   if (is.nan(ld) == TRUE) {
     ld <- .Machine$double.xmax
