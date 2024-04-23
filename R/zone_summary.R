f24dfb79338b9d3ddb6451f09c1f4398e4aef8ab
Paul-Carvalho <paul.carvalho@noaa.gov>
2024-04-23 13:37:05 -0700

    comment out parse_data_name code

--- R/zone_summary.R
+++ R/zone_summary.R
@@ -102,7 +102,7 @@ zone_summary <- function(dat,
   # Call in datasets
   out <- data_pull(dat, project)
   dataset <- out$dataset
-  # dat <- parse_data_name(dat, "main", project)
+  dat <- parse_data_name(dat, "main", project)
 
   spatout <- data_pull(spat, project)
   spatdat <- spatout$dataset
