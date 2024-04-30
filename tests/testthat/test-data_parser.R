e92d4561f281f8848f6769e2670527567c1c2970
Paul-Carvalho <paul.carvalho@noaa.gov>
2024-04-30 09:11:35 -0700

    finish unit testing for load_maindata

--- tests/testthat/test-data_parser.R
+++ tests/testthat/test-data_parser.R
@@ -1,13 +1,10 @@
+# Testing functions in data_parser.R
 
-# Test load_maindata ----
+# Test load_maindata
 test_that("Test loading main data works", {
-  
+  # Note: unable to test successful upload due to the use of rstudioapi package
   # Make sure project names that violate rules throw errors
   expect_error(load_maindata(dat = FishSET::scallop, over_write = TRUE, project = "test load"))
   expect_error(load_maindata(dat = FishSET::scallop, over_write = TRUE, project = ""))
-  
-  # 
-  # on.exit({
-  #   lapply(project_tables(project = "testload"), table_remove, project = "testload")
-  # }, add = TRUE, after = FALSE)
+  expect_error(load_maindata(dat = cbind(FishSET::scallop, FishSET::scallop), over_write = TRUE, project = "testload"))
 })
