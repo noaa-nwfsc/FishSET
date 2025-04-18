7d68af42faca550480c59cc7210174273aa9a402
Paul Carvalho <paul.carvalho@noaa.gov>
2025-04-18 13:49:57 -0700

    save barebones server file for fishset lite development

--- inst/ShinyFiles/MainApp/app.R
+++ inst/ShinyFiles/MainApp/app.R
@@ -1,14 +1,17 @@
-# ========================================================================
+# =============================================================================
 # File: app.R
 # Description: Main entry point for the FishSET GUI Shiny app.
 #              This file initializes the app by sourcing the UI and 
 #              server logic from separate files ('ui.R' and 'server.R').
+#
+# Package: FishSET
 # Authors: Paul Carvalho, Anna Abelman, et al. from previous Shiny app
 # Date created: 4/18/2025
 # Requirements: - R with Shiny package installed
 #               - ui.R and server.R in the same directory or proper paths
 # Usage: Run this file to launch the Shiny application
 #        > shiny::runApp()
+# =============================================================================
 
 # Create and run the Shiny app
 shinyApp(ui = ui, server = server, session, enableBookmarking = "server")
