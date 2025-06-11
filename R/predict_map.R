9a3d9ac424eda4694d8478558c5b82e0bf1579cf
Paul-Carvalho <paul.carvalho@noaa.gov>
2025-06-11 08:58:44 -0700

    cleaned up predict_map function code, removed unnecessary code, and added comments

--- R/predict_map.R
+++ R/predict_map.R
@@ -11,8 +11,8 @@
 #'  a csv file to an `sf` object. To upload your spatial data to the FishSETFolder 
 #'  see [load_spatial()].
 #' @param zone.spat Name of zone ID column in `spat`.
-#' @param plot_type Character, \code{"dynamic"} for interactive leaflet plots and \code{"static"}
-#'  for ggplot.
+#' @param plot_type Character, \code{"dynamic"} for interactive leaflet plots (default) and 
+#'  \code{"static"} for ggplot.
 #' @param outsample Logical, indicating if \code{predict_map()} is being used for creating map of 
 #'  out-of-sample predicted fishing probabilities \code{outsample = TRUE} or policy scenario 
 #'  \code{outsample = FALSE}.
@@ -30,109 +30,96 @@
 #' \dontrun{
 #'
 #' predict_map(project = "scallop", mod.name = "logit_c_mod1", policy.name = "closure_1", 
-#'             spat = spat, zone.spat = "TEN_ID")
+#'             spat = spat, zone.spat = "TEN_ID", plot_type = "static")
 #'
 #' }
 
-predict_map <- function(project, mod.name = NULL, policy.name = NULL, spat, 
-                        zone.spat, plot_type = "dynamic",
+predict_map <- function(project, mod.name = NULL, policy.name = NULL, 
+                        spat, zone.spat, plot_type = "dynamic", 
                         outsample = FALSE, outsample_pred = NULL){
-  
-  # Policy map ----------------------------------------------------------------------------------------------------------------
+
+  # Load project data ----------------------------------------------------------------------------
   if(!outsample){
-    ## Load prediction outputs -------------------------------------------------------------------------------------------------
-    # make sure prediction table exist in the database
-    # TODO: If this is running in the Shiny and one or more of the tables above do not exist then show error message and stop running function here
-    tryCatch({
-      model_idx <- which(lapply(model_design_list(project), "[[", "mod.name") == mod.name)
-      model_output <- model_design_list(project)[[model_idx]]},
-
-      error = function(err){message(paste0("Model output table not found in ", project))}
+    # Attempt to load the model design for mod.name
+    tryCatch(
+      {
+        model_idx <- which(lapply(model_design_list(project), "[[", "mod.name") == mod.name)
+        model_output <- model_design_list(project)[[model_idx]]
+      },
+      error = function(e){
+        message(paste0("Model output table not found in ", project))
+      }
     )
-    tryCatch({
-      pred_out <- unserialize_table(paste0(project, "predictOutput"), project)
-
-      get_mod_pred_out <- lapply(pred_out, function(x){
-        mod_name <- x$modelDat$mod.name
-        if(mod_name == mod.name){
-          return(1)
-        } else {
-          return(0)
-        }
-      })
-
-      pred_output <- pred_out[which(unlist(get_mod_pred_out) == 1)]
-    },
-    error = function(err){message(paste0("Prediction output table not found in ", project))}
+    
+    # Attempt to load policy prediction output
+    tryCatch(
+      {
+        pred_out <- unserialize_table(paste0(project, "predictOutput"), project)
+        
+        # Identify prediction output matching the specified model
+        get_mod_pred_out <- lapply(pred_out, function(x){
+          if (x$modelDat$mod.name == mod.name) return(1) else return(0)
+        })
+        
+        pred_output <- pred_out[which(unlist(get_mod_pred_out) == 1)]
+      },
+      error = function(e){
+        message(paste0("Prediction output table not found in ", project))
+      }
     )
-
+    
     # Get a list of model names from prediction output scenario names
     predict_n <- unlist(lapply(pred_output, function(x) x$scenario.name))
     mod_n <- unique(sapply(strsplit(predict_n, split = " "), "[", 1))
     model_policy_name <- paste0(mod_n, " ", policy.name)
-
+    
     if (policy.name == mod_n) {
-      # Get the index for the first prediction output for the model
-      ind <- grep(policy.name, predict_n)[1]
-
-      # Get predicted probabilities by zone for the model
-      predProbs <- pred_output[[ind]]$prob[, 1]/100
+      ind <- grep(policy.name, predict_n)[1] # Get the index for the first prediction output
+      predProbs <- pred_output[[ind]]$prob[, 1]/100 # Get predicted probabilities by zone
       probs_df <- data.frame(ZoneID = as.character(pred_output[[ind]]$zoneID),
                              Probability = predProbs)
-
+      
     } else if (model_policy_name %in% predict_n) {
-      # Get index for the prediction output for the policy
-      ind <- which(predict_n %in% model_policy_name)
-
-      # Get predicted probabilities by zone for the policy
-      predProbs <- pred_output[[ind]]$prob[, 2]/100
+      ind <- which(predict_n %in% model_policy_name) # Get index for the prediction output 
+      predProbs <- pred_output[[ind]]$prob[, 2]/100 # Get predicted probabilities by zone
       probs_df <- data.frame(ZoneID = as.character(pred_output[[ind]]$zoneID),
                              Probability = predProbs)
-
-      # Get zone ID for closured zones
-      closure <- pred_output[[ind]]$zoneIdIn
+      closure <- pred_output[[ind]]$zoneIdIn # Get zone ID for closured zones
     }
     
   } else {
-    # Out-of-sample prediction map --------------------------------------------------------------------------------------------
+    # Out-of-sample prediction map ----------------------------------------------------------------
+    # TODO: still need to develop this feature
     probs_df <- outsample_pred
   }
   
-  # Create map --------------------------------------------------------------------------------------------------------------
-  
-  # Parse spatial dataset
-  spatout <- data_pull(spat, project)
-
+  # Load and prepare spatial dataset --------------------------------------------------------------
+  spatout <- data_pull(spat, project) # Parse spatial dataset
   spatdat <- spatout$dataset
   spat <- parse_data_name(spat, "spat", project)
   spatdat[[zone.spat]] <- as.character(spatdat[[zone.spat]])
-
-  # Rename and re-type probs_df zone column
+  
+  # Prepare the probability data for joining with spatial data
   names(probs_df) <- c(zone.spat, "Probability")
   probs_df[,1] <- as.character(probs_df[,1])
-
+  
   # Merge spatial dataset with predicted probabilities
   spat_join <- dplyr::left_join(spatdat[zone.spat], probs_df, by = zone.spat)
-
-
   spat_join <-  sf::st_transform(spat_join, "+proj=longlat +datum=WGS84")
-
-
+  
+  # Define variable for dynamic mapping
   var_sym <- function() rlang::sym("Probability")
-
-  # breaks ----
-  prob_range <- range(probs_df$Probability)
+  
+  # Dynamic map (leaflet) -------------------------------------------------------------------------
   brks <- pretty(probs_df$Probability, n = 8)
   bin_colors <- fishset_viridis(length(brks))
-
   pal <- colorBin(
     bin_colors,
     bins = brks,
-    # colors depend on the count variable
-    domain = spat_join$Probability,
+    domain = spat_join$Probability # colors depend on the count variable
   )
-
-  # Plot interactive leaflet plot
+  
+  # Leaflet map
   if (plot_type == "dynamic"){
     out <- leaflet::leaflet() %>%
       leaflet::addProviderTiles("OpenStreetMap") %>%
@@ -157,39 +144,33 @@ predict_map <- function(project, mod.name = NULL, policy.name = NULL, spat,
                          values = spat_join$Probability,
                          position = "bottomright",
                          title = "Probability")
-
-    # Plot static ggplot figure
+    
+    # Static map (ggplot2) ------------------------------------------------------------------------ 
   } else if (plot_type == "static") {
-    # Save zones included in the model
-    spat_probability <- (spat_join %>% filter(!is.na(spat_join$Probability)))
-
-    # Get the bounding box
+    spat_probability <- (spat_join %>% filter(!is.na(spat_join$Probability))) # Zones in model
+    
+    # Calculate the bounding box and buffer for map aesthetics
     bbox <- st_bbox(spat_probability)
-
-    # Calculate buffer size based on the range of the coordinates
     x_buffer_ratio <- 0.6
     y_buffer_ratio <- 0.8
     x_range <- bbox$xmax - bbox$xmin
     y_range <- bbox$ymax - bbox$ymin
     x_buffer <- x_range * x_buffer_ratio
     y_buffer <- y_range * y_buffer_ratio
-
-    # Apply buffer to the bounding box
-    x_limits <- c(bbox$xmin - x_buffer, bbox$xmax + x_buffer)
-    y_limits <- c(bbox$ymin - y_buffer, bbox$ymax + y_buffer)
-
-    # Create base map for the first layer of the plot
+    x_limits <- c(bbox$xmin - x_buffer, bbox$xmax + x_buffer) # Apply buffer to the bounding box
+    y_limits <- c(bbox$ymin - y_buffer, bbox$ymax + y_buffer) 
+    
+    # Get world map and convert to sf format
     base_map <- ggplot2::map_data(map = "world",
                                   xlim = c(bbox["xmin"], bbox["xmax"]),
                                   ylim = c(bbox["ymin"], bbox["ymax"]))
-
-    # Convert base map to sf object and convert points to polygons
     base_map <- sf::st_as_sf(base_map, coords = c("long", "lat"),
                              crs = sf::st_crs(spat_join)) %>%
       dplyr::group_by(across(all_of("group"))) %>%
       dplyr::summarize(do_union = FALSE) %>%
       sf::st_cast("POLYGON")
-
+    
+    # Plot using ggplot2
     out <- ggplot() +
       geom_sf(data = base_map) +
       geom_sf(data = spat_probability, aes(fill = Probability)) +
@@ -199,6 +180,6 @@ predict_map <- function(project, mod.name = NULL, policy.name = NULL, spat,
       theme(legend.position = "inside",
             legend.position.inside = c(0.85, 0.25))
   }
-
+  
   return(out)
 }
