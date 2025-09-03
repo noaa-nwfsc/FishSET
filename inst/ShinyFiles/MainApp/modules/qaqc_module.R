c9041c820194e0eed1cd4bb56cb2ac327e5f3e78
Anna Abelman <165065127+anna-abelman@users.noreply.github.com>
2025-09-03 11:41:09 -0700

    Fsl na and nans (#317)
    * added NA/NaN module; fixing error in nan_filter()/na_filter() functions when replacing with mean; fixing qaqc_helper() to only identify NA not both NA and NaN when using "NA" function
    
    * added unit tests for nan_filter(), na_filter(), and qaqc_helper()
    
    * updated method for saving messages in na/nan function
    
    * updated unit tests with fixed na/nan_filter()
    
    * fixed code format
    
    * fixed test error in nan_filter()
    
    ---------
    
    Co-authored-by: Anna Abelman <anna.abelman>
    Co-authored-by: Paul-Carvalho <paul.carvalho@noaa.gov>

--- inst/ShinyFiles/MainApp/modules/qaqc_module.R
+++ inst/ShinyFiles/MainApp/modules/qaqc_module.R
@@ -12,6 +12,7 @@
 # Source module scripts ---------------------------------------------------------------------------
 source("modules/qaqc/preview_data_module.R", local = TRUE) # Preview data in table format
 source("modules/qaqc/summary_data_module.R", local = TRUE) # Summary stats data table
+source("modules/qaqc/remove_na_nan_module.R", local = TRUE) # Summary stats data table
 source("modules/qaqc/change_variable_module.R", local = TRUE) # Preview data in table format
 source("modules/qaqc/remove_variables_module.R", local = TRUE) # Remove variables
 source("modules/qaqc/spatial_checks_module.R", local = TRUE) # Preview data in table format
@@ -39,7 +40,10 @@ qaqc_server <- function(id, rv_project_name, rv_data, rv_folderpath){
     
     # Change variable class for primary data
     variable_class_server("change_variable_class", rv_project_name, rv_data)
-
+    
+    # Remove/replace NA/NaNs values
+    remove_na_nan_server("na_and_nan", rv_project_name, rv_data)
+    
     # Remove variables
     remove_variables_server("rm_variables", rv_project_name, rv_data, rv_folderpath)
     
@@ -71,6 +75,7 @@ qaqc_sidebar_ui <- function(id) {
                  choices = c("Preview data" = "preview", 
                              "Summary table"="summary",
                              "Change variable class" = "variable_class",
+                             "Check NAs and NaNs" = "na_nan",
                              "Remove variables" = "remove_vars",
                              "Spatial checks" = "spat_checks"),
                  selected = "preview")
@@ -103,12 +108,19 @@ qaqc_ui <- function(id){
       ns = ns,
       summary_data_ui(ns("summary_table"))
     ),
-
+    
     # Conditionally display the change class UI
     conditionalPanel(
       condition = "input.qaqc_options == 'variable_class'",
       ns = ns,
       variable_class_ui(ns("change_variable_class"))
+    ),
+    
+    # Conditionally display the NA/NaN values UI
+    conditionalPanel(
+      condition = "input.qaqc_options == 'na_nan'",
+      ns = ns,
+      remove_na_nan_ui(ns("na_and_nan"))
     ),
     
     # Conditionally display remove variables UI
