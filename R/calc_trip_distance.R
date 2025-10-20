7430c22e0854ba125ba4723a2e9e8e820aa87060
Paul-Carvalho <paul.carvalho@noaa.gov>
2025-10-20 13:24:00 -0700

    reordered calc_trip_distance inputs, also broke up start and end coords into separate lat lon inputs instead of a vector of chars

--- R/calc_trip_distance.R
+++ R/calc_trip_distance.R
@@ -1,29 +1,39 @@
 #' Calculate haul level trip distance variable
 #'
-#' @param dat Primary data containing information on hauls or trips.
-#' Table in the FishSET database contains the string 'MainDataTable'.
-#' @param project Project name. 
-#' @param port Port data frame. Contains columns: Port_Name, Port_Long, Port_Lat.
-#' Table is generated using the \code{\link{load_port}} function and saved in the FishSET database as the project and port,
-#' for example 'pollockPortTable'.
-#' @param trip_id Unique trip identifier in \code{dat}.
-#' @param starting_port Variable in \code{dat} containing ports at the start of the trip.
-#' @param starting_haul Character string, variables containing latitude and longitude at start of haul in \code{dat}.
-#' @param ending_haul Character string, variables containing latitude and longitude at end of haul in \code{dat}.
-#' @param ending_port Variable in \code{dat} containing ports at the end of the trip.
-#' @param haul_order Variable in \code{dat} that identifies haul order within a trip. Can be time, coded variable, etc.
-#' @param name String, name of created variable. Defaults to `TripDistance`.
-#' @param a  Numeric, major (equatorial) radius of the ellipsoid. The default value is for WGS84 ellipsoid.
-#' @param f  Numeric, ellipsoid flattening. The default value is for WGS84 ellipsoid.
+#' @param name String. Name of new variable for trip distance. Defaults to `TripDistance`.
+#' @param project String. project name. 
+#' @param dat String or data frame. A string for the name of the main data table in the FishSET 
+#'   database (contains 'MainDataTable' in the name). Or a data frame of the main data table.
+#' @param port String or data frame. A string for the name of the port table in the FishSET 
+#'   project database. Or a data frame of the port table. Note that the port table must at least
+#'   include a port name, port latitude and port longitude.
+#' @param trip_id String. Column name that represents the unique trip identifier in \code{dat}.
+#' @param haul_order String. Column name in \code{dat} that identifies haul order within a trip. 
+#'   Can be time, coded variable, etc.
+#' @param starting_port String. Column name in \code{dat} containing departure port for each trip.
+#' @param return_port String. Column name in \code{dat} containing landing port for each trip.
+#' @param start_haul_lat String. Column name in \code{dat} containing haul starting latitude.
+#' @param start_haul_lon String. Column name in \code{dat} containing haul starting longitude.
+#' @param end_haul_lat String. Column name in \code{dat} containing haul end latitude.
+#' @param end_haul_lon String. Column name in \code{dat} containing haul end longitude.
+#' @param a Numeric. Major (equatorial) radius of the ellipsoid. The default value is for 
+#'   WGS84 ellipsoid.
+#' @param f Numeric. Ellipsoid flattening. The default value is for WGS84 ellipsoid.
+#' 
 #' @importFrom geosphere distGeo
+#' 
 #' @export calc_trip_distance
-#' @return Returns the primary dataset with a trip distance variable added.
-#' @details Summation of distance across a trip based on starting and ending ports and hauls in between.
-#' The function uses \code{\link[geosphere]{distGeo}} from the geosphere package to calculate distances
-#'  between hauls. Inputs are the trips, ports, and hauls from the primary dataset, and the latitude and
-#'  longitude of ports from the \code{port}. The ellipsoid arguments, \code{a} and \code{f}, are numeric
-#'  and can be changed if an ellipsoid other than WGS84 is appropriate. See the geosphere R package for more details
-#'  (\url{https://cran.r-project.org/web/packages/geosphere/geosphere.pdf}).
+#' 
+#' @return Returns the main data table with a new variable for trip distance.
+#' 
+#' @details Summation of distance across a trip based on starting and ending ports and hauls in 
+#'   between. The function uses \code{\link[geosphere]{distGeo}} from the geosphere package to 
+#'   calculate distances between hauls. Inputs are the trips, ports, and hauls from the main data
+#'   table, and the latitude and longitude of ports from the port table. The ellipsoid arguments, 
+#'   \code{a} and \code{f}, are numeric and can be changed if an ellipsoid other than WGS84 is 
+#'   appropriate. See the geosphere R package for more details
+#'   (\url{https://cran.r-project.org/web/packages/geosphere/geosphere.pdf}).
+#'   
 #' @examples
 #' \dontrun{
 #' pcodMainDataTable <- calc_trip_distance(pcodMainDataTable, "pcod", "pcodPortTable", 
@@ -32,58 +42,72 @@
 #' )
 #' }
 #'
-#' #
-calc_trip_distance <- function(dat, project, port, trip_id, starting_port, starting_haul = c("Lon", "Lat"), ending_haul = c("Lon", "Lat"),
-                                 ending_port, haul_order, name = "TripDistance", a = 6378137, f = 1 / 298.257223563) {
-
-  # Call in datasets
+calc_trip_distance <- function(name = "TripDistance", 
+                               project,
+                               dat, 
+                               port, 
+                               trip_id, 
+                               haul_order,
+                               starting_port,
+                               return_port, 
+                               start_haul_lat,
+                               start_haul_lon,
+                               end_haul_lat,
+                               end_haul_lon,
+                               a = 6378137, 
+                               f = 1/298.257223563) {
+  
+  # Pull in datasets ------------------------------------------------------------------------------
   out <- data_pull(dat, project)
   dataset <- out$dataset
   dat <- parse_data_name(dat, "main", project)
   
-
   out <- data_pull(port, project)
-  port.table <- out$dataset
+  port_table <- out$dataset
   port <- parse_data_name(port, 'port', project)
-
-    if(is_empty(name)){ "TripDistance" } else {name}
- 
-  x <- 0
-
-  if (!any(unique(trimws(dataset[[ending_port]])) %in% unique(port.table[, "Port_Name"]))) {
-    warning("Ending_port from the data set and port_name from the port table do not match. Function not run.")
-    x <- 1
+  
+  # Check inputs ----------------------------------------------------------------------------------
+  if(is_empty(name)){ 
+    name <- "TripDistance" 
   }
-
-  if (!any(unique(trimws(dataset[[starting_port]])) %in% unique(port.table[, "Port_Name"])) == TRUE) {
+  
+  # Error flag
+  flag <- 0
+  
+  if (!any(unique(trimws(dataset[[return_port]])) %in% unique(port_table[["Port_Name"]]))) {
+    warning("return_port from the data set and port_name from the port table do not match. Function not run.")
+    flag <- 1
+  }
+  
+  if (!any(unique(trimws(dataset[[starting_port]])) %in% unique(port_table[, "Port_Name"])) == TRUE) {
     warning("starting_port from the data set and port_name from the port table do not match. Funciton not run.")
-    x <- 1
+    flag <- 1
   }
-
+  
   if (any(abs(dataset[, c(starting_haul)][1]) > 180) | any(abs(dataset[, c(ending_haul)][1]) > 180)) {
     warning("Longitude is not valid (outside -180:180). Function not run.")
-    x <- 1
+    flag <- 1
   }
   if (any(abs(dataset[, c(starting_haul)][2]) > 90) | any(abs(dataset[, c(ending_haul)][2]) > 90)) {
     warning("Latitude is not valid (outside -90:90. Function not run.")
-    x <- 1
+    flag <- 1
   }
-
-  if (x == 0) {
+  
+  if (flag == 0) {
     tripsFound <- unique(dataset[[trip_id]])
     C <- match(dataset[[trip_id]], tripsFound) # C = row ID of those unique items
-
+    
     startPort <- starting_port
     haulLocalStart <- starting_haul
     haulLocalEnd <- ending_haul
-    endPort <- ending_port
-
-
+    endPort <- return_port
+    
+    
     portStartidx <- rep(NA, length(tripsFound))
     portEndidx <- portStartidx
     innerHaulidx <- list() # cell(length(tripsFound),1)
     simpleidx <- list() # cell(length(tripsFound),1)
-
+    
     for (i in 1:length(tripsFound)) {
       # get all the indexing
       tripInUse <- which(C == i) # find(C==i)
@@ -94,46 +118,46 @@ calc_trip_distance <- function(dat, project, port, trip_id, starting_port, start
       innerHaulidx[[i]] <- tripInUse[id] # indexes of all values associated to a given trip in sorted order
       simpleidx[[i]] <- id # internal sorted index
     }
-
+    
     portLLS <- data.frame(matrix(NA, nrow = length(dataset[[startPort]][portStartidx]), ncol = 2))
     portLLE <- data.frame(matrix(NA, nrow = length(dataset[[endPort]][portEndidx]), ncol = 2)) #
-    temp <- sapply(trimws(dataset[[startPort]][portStartidx]), function(x) port.table[which(port.table[["Port_Name"]] == x), "Port_Long"])
+    temp <- sapply(trimws(dataset[[startPort]][portStartidx]), function(x) port_table[which(port_table[["Port_Name"]] == x), "Port_Long"])
     # a[lengths(a) == 0] <- NA_character_
     portLLS[, 1] <- as.numeric(temp) # unlist(a)
-    portLLS[, 2] <- as.numeric(sapply(trimws(dataset[[startPort]][portStartidx]), function(x) port.table[which(port.table[["Port_Name"]] == x), "Port_Lat"]))
-    portLLE[, 1] <- as.numeric(sapply(trimws(dataset[[endPort]][portEndidx]), function(x) port.table[which(port.table[["Port_Name"]] == x), "Port_Long"]))
-    portLLE[, 2] <- as.numeric(sapply(trimws(dataset[[endPort]][portEndidx]), function(x) port.table[which(port.table[["Port_Name"]] == x), "Port_Lat"]))
-
+    portLLS[, 2] <- as.numeric(sapply(trimws(dataset[[startPort]][portStartidx]), function(x) port_table[which(port_table[["Port_Name"]] == x), "Port_Lat"]))
+    portLLE[, 1] <- as.numeric(sapply(trimws(dataset[[endPort]][portEndidx]), function(x) port_table[which(port_table[["Port_Name"]] == x), "Port_Long"]))
+    portLLE[, 2] <- as.numeric(sapply(trimws(dataset[[endPort]][portEndidx]), function(x) port_table[which(port_table[["Port_Name"]] == x), "Port_Lat"]))
+    
     portToStart <- geosphere::distGeo(cbind(portLLS[, 1], portLLS[, 2]), cbind(dataset[[haulLocalStart[1]]][portStartidx], dataset[[haulLocalStart[2]]][portStartidx]),
-      a = a, f = f
+                                      a = a, f = f
     )
     portToEnd <- geosphere::distGeo(cbind(portLLE[, 1], portLLE[, 2]), cbind(dataset[[haulLocalEnd[1]]][portEndidx], dataset[[haulLocalEnd[2]]][portEndidx]),
-      a = a, f = f
+                                    a = a, f = f
     )
-
+    
     if (anyNA(match(haulLocalStart, haulLocalEnd))) {
       innerHaulDist <- geosphere::distGeo(cbind(dataset[[haulLocalStart[1]]], dataset[[haulLocalStart[2]]]), cbind(dataset[[haulLocalEnd[1]]], dataset[[haulLocalEnd[2]]]),
-        a = a, f = f
+                                          a = a, f = f
       )
     }
     # now end of haul to start of next haul..
-
+    
     haulidxall <- matrix(innerHaulidx, ncol = 1)
     haulidxall <- do.call(rbind, apply(haulidxall, 1, function(x) do.call(cbind, x)))
-
+    
     ## ---> Will need to revise based on how lat/long are indexed <----##
     endToStart <- geosphere::distGeo(cbind(dataset[[haulLocalEnd[1]]][haulidxall[1:nrow(haulidxall) - 1]], dataset[[haulLocalEnd[2]]][haulidxall[1:nrow(haulidxall) -
-      1]]), cbind(dataset[[haulLocalStart[1]]][haulidxall[-1]], dataset[[haulLocalStart[2]]][haulidxall[-1]]))
-
+                                                                                                                                                   1]]), cbind(dataset[[haulLocalStart[1]]][haulidxall[-1]], dataset[[haulLocalStart[2]]][haulidxall[-1]]))
+    
     endToStart <- c(NA, endToStart)
     # need to unsort it
     backSortId <- match(haulidxall, sort(haulidxall))
     haulEndToStart <- endToStart[backSortId]
     # replace start haul with distance from port haulEndToStart(portStartidx)=nan
     haulEndToStart[portStartidx] <- portToStart # haulEndToStart would be haul level distance variable...
-
+    
     # now try to sum all dist for one trip
-
+    
     sumToHaul <- accumarray(C, haulEndToStart) # summs the port to 1st haul + end  first haul to start of next haul
     if (any(haulLocalStart %in% haulLocalEnd)) {
       tripDist <- rowSums(cbind(sumToHaul, portToEnd), na.rm = T) # need to make a haul level tripdist variable
@@ -141,16 +165,16 @@ calc_trip_distance <- function(dat, project, port, trip_id, starting_port, start
       sumInnerDist <- accumarray(C, innerHaulDist)
       tripDist <- rowSums(cbind(sumToHaul, sumInnerDist, portToEnd), na.rm = T)
     }
-
+    
     dataset[[name]] <- tripDist[C]
-
+    
     create_TD_function <- list()
     create_TD_function$functionID <- "calc_trip_distance"
     create_TD_function$args <- list(dat, project, port, trip_id, starting_port, starting_haul, 
-                                    ending_haul, ending_port, haul_order, name, a, f)
+                                    ending_haul, return_port, haul_order, name, a, f)
     create_TD_function$output <- list(dat)
     log_call(project, create_TD_function)
-
+    
     return(dataset)
   }
 }
