8cd0cff5ac7790e7472178c7fbcbdc482da18571
Paul Carvalho <143020124+Paul-Carvalho@users.noreply.github.com>
2025-10-30 09:16:25 -0700

    Calculate trip centroid (#343)
    * started lagged zones function
    
    * change create_startingloc to lag_zone and updated the code in the function, also added unit tests and added the logic for this function in the shiny app
    
    * fix error in unit tests for lag_zone
    
    * lag_zone complete
    
    * ready for pull request
    
    * refactored haul_to_trip function and added unit tests
    
    * added haul to trip module, it works but still need to work on the UI layout
    
    * fixed ui layout
    
    * renamed create_trip_distance to calc_trip_distance
    
    * reordered calc_trip_distance inputs, also broke up start and end coords into separate lat lon inputs instead of a vector of chars
    
    * started renamed variables and sorting through the calc_trip_distance function
    
    * refactored the calc_trip_distance function, and was able to remove the accummarray function from utils which was previously used in the distance function
    
    * added globals.R file to take care of the notes from R CMD checks
    
    * added unit tests for cacl_trip_distance
    
    * added calc trip dist to shiny
    
    * minor update to cacl trip distance module comments
    
    * update test sqlite db
    
    * move create_trip_centroid to a separate R script and rename to calc_trip_centroid, and rename input args
    
    * refactored calc_trip_centroid function
    
    * added unit tests for calc_trip_centroid
    
    * removed the composite trip ID code from haul-to-trip, this should be done when creating the trip id earlier
    
    * completed cacl trip centroid function and integration into shiny
    
    * addressed comments in review of pull request - ready to merge
    
    * corrected test-calc_trip_centroid file name
    
    * fixed issues from merge conflict
    
    * update NAMESPACE
    
    ---------
    
    Co-authored-by: Paul-Carvalho <pgcarvalh@gmail.com>

--- inst/ShinyFiles/MainApp/modules/format_data/compute_new_var_module.R
+++ inst/ShinyFiles/MainApp/modules/format_data/compute_new_var_module.R
@@ -14,6 +14,7 @@ source("modules/format_data/compute_new_var/new_r_expression_module.R", local = 
 source("modules/format_data/compute_new_var/lag_zone_module.R", local = TRUE)
 source("modules/format_data/compute_new_var/haul_to_trip_module.R", local = TRUE)
 source("modules/format_data/compute_new_var/calc_trip_distance_module.R", local = TRUE)
+source("modules/format_data/compute_new_var/calc_trip_centroid_module.R", local = TRUE)
 
 # compute new variables server --------------------------------------------------------------------
 #' compute_new_var_server
@@ -39,12 +40,15 @@ compute_new_var_server <- function(id, rv_data_load_error, #values = NULL,
     
     # lag zone
     lag_zone_server("lag_zone", rv_folderpath, rv_project_name, rv_data )
-
+    
     # haul to trip
     haul_to_trip_server("haul_to_trip", rv_folderpath, rv_project_name, rv_data)
     
     # Calculate trip distance
     calc_trip_distance_server("calc_trip_dist", rv_folderpath, rv_project_name, rv_data)
+    
+    # Calculate trip centroid
+    calc_trip_centroid_server("calc_trip_cent", rv_folderpath, rv_project_name, rv_data)
   })
 }
 
@@ -65,7 +69,8 @@ compute_new_var_sidebar_ui <- function(id) {
                  choices = c("R expression" = "new_r_express",
                              "Lag zone ID" = "lag_zone_id",
                              "Haul to trip" = "haul_to_trip",
-                             "Calculate trip distance" = "calc_trip_dist"),
+                             "Calculate trip distance" = "calc_trip_dist",
+                             "Calculate trip centroid" = "calc_trip_centroid"),
                  selected = "")
   )
   
@@ -111,6 +116,13 @@ compute_new_var_ui <- function(id){
       condition = "input.comp_new_var_options == 'calc_trip_dist'", 
       ns = ns,
       calc_trip_distance_ui(ns("calc_trip_dist"))
+    ),
+    
+    # Conditionally display option to calculate trip centroid
+    conditionalPanel(
+      condition = "input.comp_new_var_options == 'calc_trip_centroid'", 
+      ns = ns,
+      calc_trip_centroid_ui(ns("calc_trip_cent"))
     )
   )
 }
