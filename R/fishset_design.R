f4eb31570be07a4546e9e59508d34dd5ae9c3266
Paul-Carvalho <paul.carvalho@noaa.gov>
2025-11-10 09:03:45 -0800

    started new model design function

---
+++ R/fishset_design.R
@@ -0,0 +1,29 @@
+
+# make_model_design <-
+#   function(project,
+#            catchID,
+#            likelihood = NULL,
+#            initparams = NULL,
+#            optimOpt = c(100, 1.0e-08, 1, 1), # tolerance may be low
+#            methodname = "BFGS",
+#            mod.name = NULL,
+#            vars1 = NULL,
+#            vars2 = NULL,
+#            priceCol = NULL,
+#            expectcatchmodels = list('all'),
+#            startloc = NULL,
+#            polyn = NULL,
+#            crs = NULL,
+#            outsample = FALSE,
+#            CV_dat = NULL) 
+
+fishset_design <- function(project,
+                           model_name,
+                           formula,
+                           ...){
+  
+  
+  
+  return(NULL)
+  
+}
