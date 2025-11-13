93df7f9c818416c606906ac4bab83f1fd9964931
Paul-Carvalho <paul.carvalho@noaa.gov>
2025-11-13 08:57:27 -0800

    started format data function

---
+++ R/format_model_data.R
@@ -0,0 +1,15 @@
+
+format_model_data <- function(project,
+                              name,
+                              select_vars = NULL,
+                              aux_data = FALSE,
+                              gridded_data = FALSE,
+                              expectations = NULL,
+                              distance = TRUE,
+                              alt_name,
+                              impute){
+  
+  
+  
+  return(NULL)
+}
