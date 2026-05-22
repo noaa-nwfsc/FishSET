74c3591f8c17091611fae58e2be7b8e3efd916be
Paul-Carvalho <paul.carvalho@noaa.gov>
2026-05-22 09:33:10 -0700

    saving progress before switching branches

--- R/run_policy.R
+++ R/run_policy.R
@@ -1,36 +1,39 @@
-#' Runs policy scenarios
+#' Run policy simulations
 #'
-#' Estimate redistributed fishing effort and welfare loss/gain from changes in policy or change in other factors that
-#' influence fisher location choice.
+#' Estimate redistributed fishing effort and welfare loss/gain from changes in policy or change in 
+#' other factors that influence fisher location choice.
 #' 
 #' @param project Name of project
-#' @param mod.name  Model name. Argument can be the name of the model or the name 
-#'   can be pulled the `modelChosen` table. Leave \code{mod.name} empty to use 
+#' @param mod_name  Model name. Argument can be the name of the model or the name 
+#'   can be pulled the `modelChosen` table. Leave \code{mod_name} empty to use 
 #'   the name of the saved `best` model. If more than one model is saved, 
-#'   \code{mod.name} should be the numeric indicator of which model to use.
+#'   \code{mod_name} should be the numeric indicator of which model to use.
 #'   Use \code{table_view("modelChosen", project)} to view a table of saved models.
-#' @param policy.name List of policy scenario names created in zone_closure function
-#' @param betadraws Integer indicating the number of times to run the welfare simulation. Default value is
-#'   \code{betadraws = 1000}
-#' @param marg_util_income For conditional and zonal logit models. Name of the coefficient to use as 
-#'   marginal utility of income.
-#' @param income_cost For conditional and zonal logit models. Logical indicating whether the coefficient
-#'    for the marginal utility of income relates to cost (\code{TRUE}) or revenue (\code{FALSE}).
-#' @param zone.dat Variable in primary data table that contains unique zone ID.
+#' @param policy_name List of policy scenario names created in zone_closure function
+#' @param betadraws Integer indicating the number of times to run the welfare simulation. 
+#'   Default value is \code{betadraws = 1000}
+#' @param marg_util_income For conditional and zonal logit models. Name of the coefficient to use 
+#'   as marginal utility of income.
+#' @param income_cost For conditional and zonal logit models. Logical indicating whether the 
+#'   coefficient for the marginal utility of income relates to cost (\code{TRUE}) or 
+#'   revenue (\code{FALSE}).
+#' @param zone_dat Variable in primary data table that contains unique zone ID.
 #' @param group_var Categorical variable from primary data table to group welfare outputs.
 #' @param enteredPrice Price data. Leave as NULL if using price data from primary 
 #'   dataset.
-#' @param expected.catch Required for conditional logit (\code{logit_c}) model.
+#' @param expected_catch Required for conditional logit (\code{logit_c}) model.
 #'   Name of expected catch table to use. Can be the expected catch from the 
 #'   short-term scenario (\code{short}), the medium-term scenario (\code{med}), 
 #'   the long-term scenario (\code{long}), or the user-defined temporal parameters 
 #'   (\code{user}).
-#' @param use.scalers Input for \code{create_model_input()}. Logical, should data be normalized? Defaults to \code{FALSE}. Rescaling factors are the mean of the 
-#' numeric vector unless specified with \code{scaler.func}.
-#' @param scaler.func Input for \code{create_model_input()}. Function to calculate rescaling factors.
-#' @details \code{run_policy} is a wrapper function for \code{\link{model_prediction}} and \code{\link{welfare_predict}}.
-#'    \code{model_prediction} estimates redistributed fishing effort after policy changes, and \code{welfare_predict}
-#'    simulates welfare loss/gain.
+#' @param use_scalers Input for \code{create_model_input()}. Logical, should data be normalized? 
+#'   Defaults to \code{FALSE}. Rescaling factors are the mean of the numeric vector unless 
+#'   specified with \code{scaler_func}.
+#' @param scaler_func Input for \code{create_model_input()}. Function to calculate rescaling 
+#'   factors.
+#' @details \code{run_policy} is a wrapper function for \code{\link{model_prediction}} and 
+#'   \code{\link{welfare_predict}}. \code{model_prediction} estimates redistributed fishing 
+#'   effort after policy changes, and \code{welfare_predict} simulates welfare loss/gain.
 #' @importFrom data.table fread
 #' @export
 
@@ -46,25 +49,35 @@
 #  welfare_predict
 #  sim_welfare
 
-run_policy <- function(project, mod.name = NULL, policy.name = NULL, betadraws = 1000, marg_util_income = NULL, 
-                       income_cost = NULL, zone.dat = NULL, group_var = NULL,
-                       enteredPrice = NULL, expected.catch = NULL, use.scalers = FALSE, scaler.func = NULL) {
+run_policy <- function(project, 
+                       mod_name = NULL, 
+                       policy_name = NULL, 
+                       betadraws = 1000, 
+                       marg_util_income = NULL, 
+                       income_cost = NULL, 
+                       zone_dat = NULL, 
+                       group_var = NULL,
+                       enteredPrice = NULL, 
+                       expected_catch = NULL, 
+                       use_scalers = FALSE, 
+                       scaler_func = NULL) {
   
   # Connect to SQL database
   fishset_db <- DBI::dbConnect(RSQLite::SQLite(), locdatabase(project = project))
   on.exit(DBI::dbDisconnect(fishset_db), add = TRUE)
   
-  # 1. Check closure file exists ----
+  # Check closure file exists ---------------------------------------------------------------------
   # Read in zone closure information
-  if (utils::file_test("-f", paste0(locoutput(project), pull_output(project, type = 'zone', fun = 'closures')))) {
+  if (utils::file_test("-f", paste0(locoutput(project), 
+                                    pull_output(project, type = 'zone', fun = 'closures')))) {
     
-    closures <- yaml::read_yaml(paste0(locoutput(project), pull_output(project, type = 'zone', fun = 'closures')))
+    closures <- yaml::read_yaml(paste0(locoutput(project), 
+                                       pull_output(project, type = 'zone', fun = 'closures')))
     
-    get_closures_out <-lapply(closures, function(x){
-      #  closures[[x]]$scenario == policy.name
+    get_closures_out <- lapply(closures, function(x) {
       pol_nm <-  x$scenario
       
-      if(pol_nm %in% policy.name){
+      if(pol_nm %in% policy_name){
         return(1)
       } else {
         return(0)
@@ -77,19 +90,18 @@ run_policy <- function(project, mod.name = NULL, policy.name = NULL, betadraws =
     stop('No policy scenario tables found. Run the zone_closure function.')
   }
   
-  
-  # 2. Check that the model can be found ----
-  # Check if mod.name from input exists in the model output list
+  # Check that the model can be found -------------------------------------------------------------
+  # Check if mod_name from input exists in the model output list
   # If the model does not exist, stop function and return error message
   if (table_exists(paste0(project, "ModelInputData"), project)) {
     
     mod.out <- model_out_view(project)
     
-    for (i in seq_along(mod.name)) { # loop through each model
+    for (i in seq_along(mod_name)) { # loop through each model
       result <- tryCatch(
         {
-          index <- grep(mod.name[i], lapply(mod.out, "[[", "name"))
-          if (length(index) == 0) stop(paste("Model output for", mod.name[i], " does not exist."))
+          index <- grep(mod_name[i], lapply(mod.out, "[[", "name"))
+          if (length(index) == 0) stop(paste("Model output for", mod_name[i], " does not exist."))
           mod.out[[index]]
         },
         error = function(e) {
@@ -104,28 +116,43 @@ run_policy <- function(project, mod.name = NULL, policy.name = NULL, betadraws =
     
   }
   
-  # 3. Run model_prediction function ----
-  model_prediction(project = project, mod.name = mod.name,
-                   closures = closures_output, enteredPrice = enteredPrice,
-                   use.scalers = use.scalers, scaler.func = scaler.func)
+  # Run model_prediction function -----------------------------------------------------------------
+  model_prediction(project = project, 
+                   mod_name = mod_name,
+                   closures = closures_output, 
+                   enteredPrice = enteredPrice,
+                   use_scalers = use_scalers, 
+                   scaler_func = scaler_func)
   
-  # 4. Run welfare predict ----
-  theta_output <-  welfare_predict(project = project, mod.name = mod.name, closures = closures_output, betadraws = betadraws,
-                                   marg_util_income = marg_util_income, income_cost = income_cost,
-                                   expected.catch = expected.catch, enteredPrice = enteredPrice)
+  # Run welfare predict ---------------------------------------------------------------------------
+  theta_output <-  welfare_predict(project = project, 
+                                   mod_name = mod_name, 
+                                   closures = closures_output, 
+                                   betadraws = betadraws,
+                                   marg_util_income = marg_util_income, 
+                                   income_cost = income_cost,
+                                   expected_catch = expected_catch, 
+                                   enteredPrice = enteredPrice)
   
-  # 6. Generate tables and plots
-  outputs_welf <-  welfare_outputs(project = project, mod.name = mod.name, closures = closures_output,
-                                   betadraws = betadraws, zone.dat = zone.dat, group_var = group_var)
+  # Generate tables and plots ---------------------------------------------------------------------
+  outputs_welf <-  welfare_outputs(project = project, 
+                                   mod_name = mod_name, 
+                                   closures = closures_output,
+                                   betadraws = betadraws, 
+                                   zone_dat = zone_dat, 
+                                   group_var = group_var)
   
-  return(list(outputs_welf, theta_output))
-  
-  ##
-  # 7. log run_policy function call ----
-  ##
+  # log run_policy function call ------------------------------------------------------------------
   run_policy_function <- list()
   run_policy_function$functionID <- "run_policy"
-  run_policy_function$args <- list(project, mod.name, enteredPrice, expected.catch, use.scalers, scaler.func)
+  run_policy_function$args <- list(project, 
+                                   mod_name, 
+                                   enteredPrice, 
+                                   expected_catch, 
+                                   use_scalers, 
+                                   scaler_func)
   run_policy_function$kwargs <- list()
   log_call(project, run_policy_function)
+  
+  return(list(outputs_welf, theta_output))
 }
